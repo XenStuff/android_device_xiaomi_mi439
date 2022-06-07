6651daee31d7603942da35f492cffda20e0e274b
xenxynon <flynryder427@gmail.com>
2022-06-07 19:48:43 +0530

    mi439 --> Mi439 ig

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/p404_mi439.mk
+    $(LOCAL_DIR)/p404_Mi439.mk
 
 COMMON_LUNCH_CHOICES := \
-    p404_mi439-user \
-    p404_mi439-userdebug \
-    p404_mi439-eng
+    p404_Mi439-user \
+    p404_Mi439-userdebug \
+    p404_Mi439-eng
--- p404_Mi439.mk
+++ p404_Mi439.mk
@@ -12,12 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/404/configs/common.mk)
 
-# Inherit from mi439 device
-$(call inherit-product, device/xiaomi/mi439/device.mk)
+# Inherit fromMi439device
+$(call inherit-product, device/xiaomi/Mi439/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := mi439
-PRODUCT_NAME := p404_mi439
+PRODUCT_DEVICE := Mi439
+PRODUCT_NAME := p404_Mi439
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SDM439
@@ -27,12 +27,12 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="mi439" \
-    PRODUCT_NAME="mi439"
+    TARGET_DEVICE="Mi439" \
+    PRODUCT_NAME="Mi439"
 
 # 404
 ifeq ($(WITH_GAPPS),true)
 P404_BUILDTYPE := SHINKA-GAPPS
 else
 P404_BUILDTYPE := SHINKA-VANILLA
-endif 
+endif
